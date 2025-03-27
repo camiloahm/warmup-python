120ea29d46bae349732161d5eabc0849138d7bfc
Camilo_Hernandez <camilo.hernandez@ihg.com>
2025-03-27 15:47:17 -0400

    ProductOfArrayExceptSelf

--- problem/ProductOfArrayExceptSelf.py
+++ problem/ProductOfArrayExceptSelf.py
@@ -13,7 +13,7 @@ class ProductOfArrayExceptSelf:
 
     def bruteProductOfArrayExceptSelf(self, nums: List[int]) -> List[int]:
 
-        result = [int]
+        result: List[int] = []
         for i in range(len(nums)):
             helper = 1
             for j in range(len(nums)):
@@ -26,18 +26,40 @@ class ProductOfArrayExceptSelf:
     def productOfArrayExceptSelf(self, nums: List[int]) -> List[int]:
         left = {}
         right = {}
-        return None
+        result = []
+
+        for i in range(len(nums)):
+            if i == 0:
+                left[i] = 1
+            else:
+                left[i] = left[i - 1] * nums[i - 1]
+
+        for i in reversed(range(len(nums))):
+            if i == len(nums) - 1:
+                right[i] = 1
+            else:
+                right[i] = right[i + 1] * nums[i + 1]
+
+        for i in range(len(nums)):
+            result.append(left[i] * right[i])
+
+        return result
 
 
 if __name__ == '__main__':
     solution = ProductOfArrayExceptSelf()
 
-    print(solution.bruteProductOfArrayExceptSelf([1, 2, 3, 4]))
-    print(solution.bruteProductOfArrayExceptSelf([-1, 1, 0, -3, 3]))
-    assert solution.bruteProductOfArrayExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
-    assert solution.bruteProductOfArrayExceptSelf([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
-    #
-    # logging.info(solution.productOfArrayExceptSelf([1, 2, 3, 4]))
-    # logging.info(solution.productOfArrayExceptSelf([-1, 1, 0, -3, 3]))
+    resultTestCase1 = solution.bruteProductOfArrayExceptSelf([1, 2, 3, 4])
+    resultTestCase2 = solution.bruteProductOfArrayExceptSelf([-1, 1, 0, -3, 3])
+    print(resultTestCase1)
+    print(resultTestCase2)
+
+    expectedTestCase1: List[int] = [24, 12, 8, 6]
+    assert resultTestCase1 == expectedTestCase1, f"Elements are not equal {resultTestCase1} != {expectedTestCase1}"
+    expectedTestCase2: List[int] = [0, 0, 9, 0, 0]
+    assert resultTestCase2 == expectedTestCase2, f"Elements are not equal {resultTestCase2} != {expectedTestCase2}"
+
+    print(solution.productOfArrayExceptSelf([1, 2, 3, 4]))
+    print(solution.productOfArrayExceptSelf([-1, 1, 0, -3, 3]))
     # assert solution.productOfArrayExceptSelf([1, 2, 3, 4]) == [24, 12, 8, 6]
     # assert solution.productOfArrayExceptSelf([-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
